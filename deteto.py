2e4de3227ace8afc925c816b206fa9c4a5d971df
Karthick Nagarajan <karthick965938@gmail.com>
2020-05-11 22:45:34 +0530

    first commit

---
+++ deteto.py
@@ -0,0 +1,5 @@
+from detecto import core, utils, visualize
+image = utils.read_image('cat.jpg')
+model = core.Model() 
+labels, boxes, scores = model.predict_top(image)
+visualize.show_labeled_image(image, boxes, labels)
